720a8bf723a18f8b3b694453c497fc2b6921e77f
subham <beherasubham253@gmail.com>
2026-02-03 11:24:31 +0530

    Initial Java hello world app

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World from Jenkins Pipeline!");
+        try {
+            Thread.sleep(10000);
+        } catch (Exception e) {}
+    }
+}
